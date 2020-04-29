0617663c62fbd46ca23144e59981f753e6082c31
syzymon <s.tworkowski@student.uw.edu.pl>
2020-04-29 11:41:22 +0200

    Finished initial styling and HTML, init js

---
+++ src/data/questions.ts
@@ -0,0 +1,28 @@
+interface Question {
+  readonly statement: string;
+  readonly answer: number;
+  readonly secondsPenalty: number;
+}
+
+export const questions = [
+  {
+    statement: '(!+[]+[]+![]).length',
+    answer: 9,
+    secondsPenalty: 10,
+  },
+  {
+    statement: '"2"+"2"-"2"',
+    answer: 20,
+    secondsPenalty: 7,
+  },
+  {
+    statement: '11-"1"',
+    answer: 10,
+    secondsPenalty: 4,
+  },
+  {
+    statement: '{}+[]',
+    answer: 0,
+    secondsPenalty: 5,
+  },
+] as Question[];
